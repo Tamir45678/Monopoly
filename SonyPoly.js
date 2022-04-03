3dbe5b73830c5c78ba5d446856243aa0baa7e325
Tamir45678 <79155637+Tamir45678@users.noreply.github.com>
2022-04-03 09:49:24 +0300

    monopoly

---
+++ SonyPoly.js
@@ -0,0 +1,584 @@
+﻿Monopoly = {
+    sonyPoly: [
+        {
+            "id":1,
+            "Category": "Missions",
+            "Name": "Game On",
+            "imgsrc": "sonypolyimages/gameOn.jpg"
+
+        },
+        {
+            "id":2,
+            "Category": "Family",
+            "Name": "Flying Soldiers",
+            "Price": 250,
+            "imgsrc": "sonypolyimages/flyingSoldiers.jpg"
+        },
+        {
+            "id":3,
+            "Category": "Family",
+            "Name": "AlphaSet",
+            "Price": 300,
+            "imgsrc": "sonypolyimages/Alphaset.jpg"
+        },
+        {
+            "id":4,
+            "Category": "Missions",
+            "Name": "Gift Card",
+            "imgsrc": "sonypolyimages/giftbox.png"
+        },
+        {
+            "id":5,
+            "Category": "Family",
+            "Name": "Millionaire",
+            "Price": 400,
+            "imgsrc": "sonypolyimages/whoWantsToBeAMillionaire.jpg"
+        },
+        {
+            "id":6,
+            "Category": "Missions",
+            "Name": "Ban - visitor",
+            "imgsrc": "sonypolyimages/ban.jpg"
+        },
+        {
+            "id":7,
+            "Category": "Racing",
+            "Name": "Need for Speed",
+            "Price": 500,
+            "imgsrc": "sonypolyimages/needForSpeed.jpg"
+        },
+        {
+            "id": 8,
+            "Category": "Racing",
+            "Name": "Grand Turisimo",
+            "Price": 800,
+            "imgsrc": "sonypolyimages/GrandTurisimo.jpg"
+        },
+        {
+            "id": 9,
+            "Category": "Sport",
+            "Name": "Rocket League",
+            "Price": 1000,
+            "imgsrc": "sonypolyimages/RocketLeague.jpg"
+        },
+        {
+            "id": 10,
+            "Category": "Sport",
+            "Name": "NBA 2K21",
+            "Price": 1200,
+            "imgsrc": "sonypolyimages/NBA2K21.jpg"
+        },
+        {
+            "id": 11,
+            "Category": "Missions",
+            "Name": "Rest",
+            "imgsrc": "sonypolyimages/rest.jpg"
+        },
+        {
+            "id": 12,
+            "Category": "Sport",
+            "Name": "Fifa 21",
+            "Price": 1100,
+            "imgsrc": "sonypolyimages/Fifa21.jpg"
+        },
+        {
+            "id": 13,
+            "Category": "Missions",
+            "Name": "Payment",
+            "imgsrc": "sonypolyimages/payFine.jpg"
+        },
+        {
+            "id": 14,
+            "Category": "Fighting",
+            "Name": "Street Fighter",
+            "Price": 1300,
+            "imgsrc": "sonypolyimages/StreetFighter.jpg"
+        },
+        {
+            "id": 15,
+            "Category": "Fighting",
+            "Name": "Tekken",
+            "Price": 2000,
+            "imgsrc": "sonypolyimages/Tekken.jpg"
+        },
+        {
+            "id": 16,
+            "Category": "Missions",
+            "Name": "Get Ban",
+            "imgsrc": "sonypolyimages/goToBan.jpg"
+        },
+        {
+            "id": 17,
+            "Category": "Shooting",
+            "Name": "Modern Warfare",
+            "Price": 2200,
+            "imgsrc": "sonypolyimages/ModernWarfare.jpg"
+        },
+        {
+            "id": 18,
+            "Category": "Shooting",
+            "Name": "Cold  War",
+            "Price": 2300,
+            "imgsrc": "sonypolyimages/ColdWar.jpg"
+        },
+        {
+            "id": 19,
+            "Category": "Missions",
+            "Name": "Money",
+            "imgsrc": "sonypolyimages/money.jpg"
+        },
+        {
+            "id": 20,
+            "Category": "Shooting",
+            "Name": "Warzone",
+            "Price": 3000,
+            "imgsrc": "sonypolyimages/Warzone.jpg"
+        }
+    ],
+
+    //Creating monoPort dataset
+    monoPort: [
+        {
+            "id": 1,
+            "Category": "Missions",
+            "Name": "Game On",
+            "imgsrc": "monoportimages/gameOn.png"
+
+        },
+        {
+            "id": 2,
+            "Category": "Africa",
+            "Name": "מדגסקר",
+            "Price": 250,
+            "imgsrc": "monoportimages/madagascar.png"
+        },
+        {
+            "id": 3,
+            "Category": "Africa",
+            "Name": "זימבבואה",
+            "Price": 300,
+            "imgsrc": "monoportimages/zimbabwe.jpg"
+        },
+        {
+            "id": 4,
+            "Category": "Missions",
+            "Name": "Gift Card",
+            "imgsrc": "monoportimages/airlineTicket.png"
+        },
+        {
+            "id": 5,
+            "Category": "Africa",
+            "Name": "טנזניה",
+            "Price": 400,
+            "imgsrc": "monoportimages/tanzania.png"
+        },
+        {
+            "id": 6,
+            "Category": "Missions",
+            "Name": "validatePassport",
+            "imgsrc": "monoportimages/passportApproved.jpg"
+        },
+        {
+            "id": 7,
+            "Category": "Europe",
+            "Name": "יוון",
+            "Price": 500,
+            "imgsrc": "monoportimages/greece.png"
+        },
+        {
+            "id": 8,
+            "Category": "Europe",
+            "Name": "צרפת",
+            "Price": 800,
+            "imgsrc": "monoportimages/france.png"
+        },
+        {
+            "id": 9,
+            "Category": "Europe",
+            "Name": "הולנד",
+            "Price": 1000,
+            "imgsrc": "monoportimages/netherlands.png"
+        },
+        {
+            "id": 10,
+            "Category": "Europe",
+            "Name": "ספרד",
+            "Price": 1200,
+            "imgsrc": "monoportimages/spain.png"
+        },
+        {
+            "id": 11,
+            "Category": "Missions",
+            "Name": "Rest",
+            "imgsrc": "monoportimages/rest.jpg"
+        },
+        {
+            "id": 12,
+            "Category": "Asia",
+            "Name": "סין",
+            "Price": 1100,
+            "imgsrc": "monoportimages/china.png"
+        },
+        {
+            "id": 13,
+            "Category": "Missions",
+            "Name": "Overweight baggage",
+            "imgsrc": "monoportimages/baggage.png"
+        },
+        {
+            "id": 14,
+            "Category": "Asia",
+            "Name": "הודו",
+            "Price": 1300,
+            "imgsrc": "monoportimages/india.png"
+        },
+        {
+            "id": 15,
+            "Category": "Asia",
+            "Name": "תאילנד",
+            "Price": 2000,
+            "imgsrc": "monoportimages/thailand.png"
+        },
+        {
+            "id": 16,
+            "Category": "Missions",
+            "Name": "Passport Rejected",
+            "imgsrc": "monoportimages/passportRejected1.jpg"
+        },
+        {
+            "id": 17,
+            "Category": "America",
+            "Name": "ברזיל",
+            "Price": 2200,
+            "imgsrc": "monoportimages/brasil.png"
+        },
+        {
+            "id": 18,
+            "Category": "America",
+            "Name": "ארגנטינה",
+            "Price": 2300,
+            "imgsrc": "monoportimages/argentina.png"
+        },
+        {
+            "id": 19,
+            "Category": "Missions",
+            "Name": "Frequent flyer",
+            "imgsrc": "monoportimages/frequentFlyer.png"
+        },
+        {
+            "id": 20,
+            "Category": "America",
+            "Name": "ארצות הברית",
+            "Price": 3000,
+            "imgsrc": "monoportimages/usa.png"
+        }
+    ]
+}
+
+//getting data from Local Storage
+currentGame = localStorage["dataset"];
+players = JSON.parse(localStorage[currentGame]);
+var playerNum = null;
+var buyOption = null;
+var choose = null;
+
+// Function making the expected game board.
+function addToStation(a) {
+
+    for (i = 0; i < a.length; i++) {
+        var str = "";
+        if (i == 0) {
+            str += '<div class="card" id="startPoint"><img class="card-img" src="' + a[i].imgsrc + '"/></div>'
+        }
+        else if (i == 5) {
+            str += '<div class="card jail"><img class="card-img" id="jail" src="' + a[i].imgsrc + '"/></div>'
+        }
+        else if (a[i].Category != "Missions")
+            str += '<div class="card"><div class="card-header ' + a[i].Category + '"> </div> <div class="card-title">' + a[i].Name + ' </div><img class="card-img" src="' + a[i].imgsrc + '"/> <div class="card-subtitle">' + a[i].Price + '₪</div></div>';
+
+        else
+            str = '<div class="card missionCards "><div class="card-title">' + a[i].Name + ' </div><img class="card-img missions" src="' + a[i].imgsrc + '"/></div>'
+
+        document.getElementById("station" + (i + 1)).innerHTML = str;
+    }
+    for (let i = 0; i <= 1; i++) {
+        updatePlayersStation(i);
+        renderPlayerData(i);
+    }
+    
+    if (localStorage[currentGame] != null)
+        markAsOwned();
+}
+
+//Updateing the player location 
+function updatePlayersStation(playerNum) {
+    document.getElementById("station" + players[playerNum].station).getElementsByClassName("card")[0].innerHTML += "<img src='" + players[playerNum].image + "' id = 'playerimg" + (playerNum + 1) + "'/>";
+    }
+
+
+
+
+
+
+// Rendering the supplied player data
+function renderPlayerData(playerNum) {
+        renderData = "<h3>" + players[playerNum].name + "</h3> <p> Money: " + players[playerNum].budget + "</p> <p> Stations Owned: ";
+        for (j = 0; j< players[playerNum].stationOwn.length; j++)
+            renderData += players[playerNum].stationOwn[j].Name + ", ";
+        renderData += "</p> <p>Total Equity: " + (players[playerNum].equity + players[playerNum].budget) + "</p>"
+        document.getElementById("dataPlayer" +(playerNum+1)).innerHTML = renderData;
+    }
+
+// Removing the expected item
+function removeItem(itemID) {
+    o = document.getElementById(itemID);
+    parent = o.parentNode;
+    parent.removeChild(o);
+}
+
+$(document).ready(init);
+
+
+// Returns the current player number 
+function currentPlayer() {
+    if (players[0].turn == true)
+        return  0;
+    return 1;
+}
+
+// Marks the owned stations of the players by the appropriate color
+function markAsOwned() {
+    for (var i = 0; i < players[0].stationOwn.length; i++)
+        $("#station" + players[0].stationOwn[i].id).css(checkStationSide(players[0].stationOwn[i].id), "10px solid red" );
+    for (var i = 0; i < players[1].stationOwn.length; i++)
+        $("#station" + players[1].stationOwn[i].id).css(checkStationSide(players[1].stationOwn[i].id), "10px solid blue");
+
+}
+
+// Checks where is the station location in order to add an appropriate border while purchasing
+function checkStationSide(station) {
+    if (station > 1 && station < 6)
+        return "border-top";
+    if (station > 6 && station < 11)
+        return "border-right";
+    if (station > 11 && station < 16)
+        return "border-bottom";
+    return "border-left";
+}
+
+function init() {
+
+
+    //Roll the dice method
+    $(".dice").click(function () {
+        result = rollDice();
+        //$("#result").html(result);
+        playerNum = currentPlayer();
+        //lastStation = players[playerNum].station;
+        stationCheck = players[playerNum].station + result;
+        removeItem("playerimg" + (playerNum + 1));
+        if (players[playerNum].isJail != true) {
+            if (stationCheck > 20) {
+                alertify.alert("You Pass the Starting Line again! You got rewarded for that. Keep going!").setHeader("<h3>Starting line pass!</h3>");
+                players[playerNum].station = stationCheck - 20;
+                players[playerNum].budget += 200;
+            }
+            else
+                players[playerNum].station += result;
+
+            turnAction(players[playerNum].station);
+        }
+        else if (players[playerNum].rollCount == 2 || result == 6) {
+            players[playerNum].isJail = false;
+            players[playerNum].rollCount = 0;
+        }
+        else
+            players[playerNum].rollCount++;
+        updatePlayersStation(playerNum);
+            for (var i = 0; i <= 1; i++) {
+                renderPlayerData(i);
+                players[i].turn = !(players[i].turn);      
+        }
+        localStorage[currentGame] = JSON.stringify(players);
+    });
+
+    // Check whether the station is owned or for sale
+    function isOwned(station) {
+        x = document.getElementById("station"+station).getAttribute("style");
+        if (x != null) {
+                if (currentPlayer() == 1) 
+                    return x.includes("10px solid red");
+            return x.includes("10px solid blue");
+        }
+    }
+
+    //Handles the current player turn options
+    function turnAction(station) {
+        currentStation = Monopoly[currentGame][station - 1];
+        stationsOwned = players[playerNum].stationOwn;
+        check = document.getElementById("station" + station).innerHTML;
+        
+        if (!check.includes("missionCards") && !check.includes("jail") &&!check.includes("startPoint")) {
+            for (var i = 0; i < stationsOwned.length; i++) {
+                if (stationsOwned[i].Name == currentStation.Name)
+                    return;
+            }
+            if (isOwned(station))
+                payStation(station);
+            else {
+                buyStation(station);
+            }
+        }
+        else
+            missionsCells();
+
+    }
+
+    // Function that handles the missions cells
+    function missionsCells() {
+        switch (players[playerNum].station) {
+            case 4: {
+                gift = (Math.floor(Math.random() * 3) + 1) * 100;
+                alertify.alert("You get a nice reward into your equity. You're reward is: " + gift + "₪. Enjoy!").setHeader("<h3>Equity Reward!</h3>");
+                players[playerNum].equity += gift;
+                break;
+            }
+            case 6: {
+                alertify.alert("Welcome! It is so nice you came to visit.").setHeader("<h3>Jail Visit!</h3>");
+                break;
+            }
+            case 11: {
+                alertify.alert("No hurry. Sometimes you need a break. You're welcome!").setHeader("<h3>Rest!</h3>");
+                break;
+            }
+            case 13: {
+                if (players[playerNum].budget >= 10) {
+                    fine = (Math.floor(Math.random() * 10) + 1) * 10
+                    while (fine > players[playerNum].budget)
+                        fine = (Math.floor(Math.random() * 10) + 1) * 10
+                    alertify.alert("Aww. You need to pay us. Your fine is: " + fine).setHeader("<h3>You got fined!</h3>");
+                    players[playerNum].budget -= fine;
+                }
+                break;
+              
+            }
+            case 16: {
+                if (players[playerNum].budget >= 250) {
+                    alertify.confirm("<h3>Chance for jail escape!</h3>","You cant play for 3 turns now. do you want to pay 250₪ for quick release?",
+                        function () {
+                            alertify.alert("You free from now.").setHeader("Escape");
+                            players[playerNum].budget -= 250;
+                        },
+                        function () {
+                            sendToJail(playerNum);
+                        });
+                }
+                else {
+                    sendToJail(playerNum);
+                }
+                break;
+            }
+            case 19: {
+                money = (Math.floor(Math.random() * 2) + 1) * 100;
+                alertify.alert("Hey man! You get a nice reward of " + money + "₪. Enjoy!").setHeader("<h3>Budget Reward!</h3>");
+                players[playerNum].budget += money
+                break;
+            }
+            default: {
+                alertify.alert("You step on the Starting Line! 400₪ for you that round!").setHeader("<h3>Starting line stand!</h3>");
+                players[playerNum].budget += 200;
+            }
+
+        }           
+    }
+
+    // Jail method
+    function sendToJail(playerNum) {
+        alertify.alert("So, you are out for 3 turns.if you get a 6 in our of you dice roll, you will be free sooner. See you soon!").setHeader("<h3>Jail Time!</h3>");
+        removeItem("playerimg" + (playerNum + 1));
+        players[playerNum].station = 6;
+        players[playerNum].isJail = true;
+        players[playerNum].rollCount = 0;
+        updatePlayersStation(playerNum);
+    }
+
+    // Buying stations method
+    function buyStation(station) {
+        playerNum = currentPlayer();
+        currentStation = Monopoly[currentGame][station - 1];
+        if (currentStation.Price <= players[playerNum].budget) {
+            alertify.confirm("Do you want to buy this station? you have enough Money!", function (e) {
+                if (e) {
+                    players[playerNum].budget -= currentStation.Price;
+                    players[playerNum].stationOwn.push(currentStation);
+                    players[playerNum].equity += (currentStation.Price) / 2;
+                    strSide = checkStationSide(station);
+                    renderPlayerData(playerNum);
+                    if (playerNum == 1)
+                        $("#station" + station).css( strSide,"10px solid blue" );
+                    else
+                        $("#station" + station).css(strSide, "10px solid red");
+                }
+                else
+                    alertify.alert("You better buy it next time!");
+            }).setHeader("<h3>Option to Buy</h3>");
+         }
+    }
+
+      // Pay to station owner method
+    function payStation(station) {
+        payment = Math.floor((Monopoly[currentGame][station - 1].Price) / 10);
+        players[currentPlayer()].budget -= payment;
+        if (currentPlayer() == 1) {
+            players[0].budget += payment;
+        }
+        else
+            players[1].budget += payment;
+        alertify.alert("You step on station owned by other player. you got to payed: " + payment + "₪.").setHeader("<h3>Payment</h3>");
+    }
+
+
+    //End game button(image) click event
+    $("#endGame").click(function () {
+        str = "<h2>The winner is: ";
+        player1total = players[0].budget + players[0].equity;
+        player2total = players[1].budget + players[1].equity;
+        if (player1total > player2total)
+            str += players[0].name + "!";
+        else if (player2total > player1total)
+            str += players[1].name + "!";
+        else
+            str = "Its a Tie!";
+        str += "</h2>";
+        localStorage.removeItem(currentGame);
+        //alertify.alert(str + '<br/><img id="winner" src="sonypolyimages/monopolywinner.gif"/>', function (e) {
+        //    if (e) {
+        //        window.location.replace('welcomePage.html');
+        //    }
+        //}).setHeader("Game-Over");
+        $.alert({
+            title: "Game-Over!",
+            content: str + '<br/><img id="winner" src="sonypolyimages/monopolywinner.gif"/>',
+            onAction: function () {
+                window.location.replace('welcomePage.html');
+            },
+            theme: "supervan",
+        });
+    });
+
+
+
+    // hover and unhover functions for end game image
+    $("#endGame").hover(function () {
+        $("#endGame").css({ "right": "0px", "position": "absolute", "top": "0px", "height": "30%", "width": "30%", "cursor": "pointer" })
+    });
+
+    $("#endGame").mouseleave(function () {
+        $("#endGame").removeAttr("style");
+    })
+
+
+    //var missions = $.alert({
+
+    //})
+}
---
+++ rollDiceJs.js
@@ -0,0 +1,23 @@
+﻿
+
+function rollDice() {
+    const dice = [...document.querySelectorAll(".die-list")];
+    dice.forEach(die => {
+        toggleClasses(die);
+        diceNumber = getRandomNumber(1, 6);
+        die.dataset.roll = diceNumber;
+    });
+        return diceNumber;
+}
+
+
+function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
